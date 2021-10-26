ff72e9dc21ccf54968a2947a287133988205e792
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-10-26 16:42:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import streamlit as st
+
+header = st.container()
+dataset= st.container()
+features = st.container()
+modelTraining = st.container()
+
+with header:
+    st.title("Welcome")
