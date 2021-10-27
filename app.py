319ea6cef7bea4cd8dc776b4d3c9c5e49a09d5fb
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-10-27 10:13:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
-header = st.container()
-dataset= st.container()
-features = st.container()
-modelTraining = st.container()
-
-with header:
-    st.title("Welcome to my new web app")
-    st.write('This is a table')
-    dataframe = pd.DataFrame(np.random.randn(10, 20),
-                             columns = ('col %d' % i
-                                        for i in range(20)))
+st.title("Welcome to my new web app")
+st.write('This is a table')
+dataframe = pd.DataFrame(np.random.randn(10, 20),
+    columns = ('col %d' % i
+        for i in range(20)))
 st.write(dataframe)
 st.write('This is a line_chart.')
 st.line_chart(dataframe)
+
 
