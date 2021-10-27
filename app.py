547bf2e7e3e98c3285a85e6263e002e7e0794d2d
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-10-27 10:02:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ This is a temporary script file.
 """
 
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 
 header = st.container()
 dataset= st.container()
@@ -14,4 +17,11 @@ modelTraining = st.container()
 
 with header:
     st.title("Welcome to my new web app")
-    
+    st.write('This is a table')
+    dataframe = pd.DataFrame(np.random.randn(10, 20),
+                             columns = ('col %d' % i
+                                        for i in range(20)))
+st.write(dataframe)
+st.write('This is a line_chart.')
+st.line_chart(dataframe)
+
